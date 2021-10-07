b29ef32ad86e1f2c018fc883587904b0a031db01
Nik Pirc <nik@oaa.si>
2021-10-07 18:18:49 +0200

    initial commit

---
+++ prvi program lekcije 1/main.py
@@ -0,0 +1,33 @@
+x = 5
+y = 3
+z = 77
+
+# float
+a = 10.74
+
+# string
+ime = "Nik"
+vreme = "vreme je slabo"
+
+print(ime + " " + vreme)
+print(x + y + z + a)
+
+# Boolean
+dezuje = False
+
+# nothing
+poplave = None
+
+
+name = input("What's your name")
+
+prva = int(input("Prva stevilka"))
+druga = int(input("druga stevilka"))
+
+print(prva * druga)
+
+mood = "happy"
+ if mood == "happy":
+     print("happpppyyy")
+else:
+    print("sej bo")
